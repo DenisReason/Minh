a8c5355a10a4235808f28a37c049af20e679f0d5
MinhTri <str.comparetoignore@gmail.com>
2023-11-16 00:12:15 +0700

    renewday11

---
+++ Day 11 Native/Login/App.js
@@ -0,0 +1,53 @@
+
+import axios from 'axios';
+import React,{useState }from 'react';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+
+
+const reqLogintoServer = async(data,settoken)=>{
+  axios.post('https://day10-daa5ba58a880.herokuapp.com/Login', data)
+  .then(Response=>{
+    settoken(Response.data)
+  })
+  .catch(error=>{
+    if(error.response.data == 'Account does not exist'){
+      settoken(error.response.data)
+    }
+    if(error.response.data=='Incorrect account name or password'){
+      settoken(error.response.data)
+    }
+    console.log('====================================');
+    console.log(error);
+    console.log('====================================');
+  })
+}
+
+export default function App() {
+  const [usersname, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [token, settoken] = useState('')
+  
+  return (
+    <View style={styles.container}>
+      <TextInput style={styles.input} placeholder='User name' onChangeText={setUsername} value={usersname}></TextInput>
+      <TextInput style={styles.input} placeholder='Password' onChangeText={setPassword} value={password}></TextInput>
+      <Button onPress={()=>{reqLogintoServer({username:usersname,password:password},settoken)}} title='Login'></Button>
+      {token && (<Text>{(JSON.stringify(token).replaceAll('"',""))}</Text>)}
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input:{
+    borderColor:"black",
+    borderWidth:1,
+    margin:5 ,
+    width: 300,
+    padding:5
+  }
+})
---
+++ Day 11 Native/Login/README.txt
@@ -0,0 +1,5 @@
+// TỔNG KẾT BÀI
+// COMMAND ĐỂ CHẠY 
+// npx expo start --tunnel
+// npx create-expo-app <name>
+// HỌC VỀ CÚ PHÁP short circul eluation
