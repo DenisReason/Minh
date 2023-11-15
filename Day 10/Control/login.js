fa815deea3fd2cb9186a183c53ff7ec57d23e53b
MinhTri <str.comparetoignore@gmail.com>
2023-11-16 00:15:05 +0700

    delete

--- Day 10/Control/login.js
+++ Day 10/Control/login.js
@@ -4,25 +4,20 @@ import { GetuserByUsername } from '../Model/ConnectToDb.js'
 import { genToken } from '../Model/jwt.js'
 export const login = express.Router()
 
-
-
-login.post("/login", async (req, res, next) => {
+login.post("/login", async(req, res, next)=>{
     const User = await GetuserByUsername(req.body.username)
-    if (!User) {
-        res.status(401).send('Account does not exist')
+    console.log(User);
+    if(!User){
+        res.status(401).send("Account does not exist")
         return
     }
     const Result = await bcrypt.compare(req.body.password, User.password)
-
-    if (Result) {
+    if(Result){
         const token = await genToken(req, res, next)
-        res.setHeader('Authorization', `Bearer ${token}`)
         res.status(200).send(token)
-        
     }
-
-    else {
-        res.status(500).send("Incorrect account name or password")
+    else{
+        res.send("Incorrect account name or password")
     }
 })
 
--- Day 11 Native/project2/README.txt
+++
@@ -1,6 +0,0 @@
-// TỔNG KẾT BÀI
-// COMMAND ĐỂ CHẠY 
-// npx expo start --tunnel
-// npx create-expo-app <name>
-// HỌC VỀ CÚ PHÁP short circul eluation
-// 
